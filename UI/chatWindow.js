413c77a107e8111078a821827b1ae5dff8879e2e
AnuragKule <89693418+AnuragKule@users.noreply.github.com>
2025-02-03 17:22:19 +0530

    Update chatWindow.js

--- UI/chatWindow.js
+++ UI/chatWindow.js
@@ -4115,40 +4115,84 @@
 		    ${helpers.formatDate(msgData.createdOn)}</div>\
 	    {{/if}} \
             </scipt>';
-
-            var quickReplyTemplate = '<script id="chat_message_tmpl" type="text/x-jqury-tmpl"> \
-                {{if msgData.message}} \
-                    <li data-time="${msgData.createdOnTimemillis}" id="${msgData.messageId || msgItem.clientMessageId}"\
-                        class="{{if msgData.type === "bot_response"}}fromOtherUsers{{else}}fromCurrentUser{{/if}} with-icon quickReplies"> \
-                        <div class="buttonTmplContent"> \
-                            {{if msgData.icon}}<div aria-live="off" class="profile-photo"> <div class="user-account avtar marginT50" style="background-image:url(${msgData.icon})"></div> </div> {{/if}} \
-                            {{if msgData.message[0].component.payload.text}} \
-                                <div class="buttonTmplContentHeading quickReply"> \
-                                    {{if msgData.type === "bot_response"}} {{html helpers.convertMDtoHTML(msgData.message[0].component.payload.text, "bot")}} {{else}} {{html helpers.convertMDtoHTML(msgData.message[0].component.payload.text, "user")}} {{/if}} \
-                                    {{if msgData.message[0].cInfo && msgData.message[0].cInfo.emoji}} \
-                                        <span class="emojione emojione-${msgData.message[0].cInfo.emoji[0].code}">${msgData.message[0].cInfo.emoji[0].title}</span> \
-                                    {{/if}} \
-                                </div>\
+	//Anurag - 03/02/2025
+	var quickReplyTemplate = '<script id="chat_message_tmpl" type="text/x-jqury-tmpl"> \
+            {{if msgData.message}} \
+                <li data-time="${msgData.createdOnTimemillis}" id="${msgData.messageId || msgItem.clientMessageId}"\
+                    class="{{if msgData.type === "bot_response"}}fromOtherUsers{{else}}fromCurrentUser{{/if}} with-icon quickReplies"> \
+                    <div class="buttonTmplContent"> \
+                        {{if msgData.icon}}\
+                            <div aria-live="off" class="profile-photo"> \
+                                <div class="user-account avtar marginT50" style="background-image:url(${msgData.icon})"></div> \
+                            </div> \
+                        {{/if}} \
+                        {{if msgData.message[0].component.payload.text}} \
+                            <div class="buttonTmplContentHeading quickReply"> \
+                                {{if msgData.type === "bot_response"}} \
+                                    {{html helpers.convertMDtoHTML(msgData.message[0].component.payload.text, "bot")}} \
+                                {{else}} \
+                                    {{html helpers.convertMDtoHTML(msgData.message[0].component.payload.text, "user")}} \
                                 {{/if}} \
-                                {{if msgData.message[0].component.payload.quick_replies && msgData.message[0].component.payload.quick_replies.length}} \
-                                <div class="fa fa-chevron-left quickreplyLeftIcon hide"></div><div class="fa fa-chevron-right quickreplyRightIcon"></div>\
-                                    <div class="quick_replies_btn_parent"><div class="autoWidth">\
-                                        {{each(key, msgItem) msgData.message[0].component.payload.quick_replies}} \
-                                            <div class="buttonTmplContentChild quickReplyDiv"> <span {{if msgItem.payload}}value="${msgItem.payload}"{{/if}} class="quickReply {{if msgItem.image_url}}with-img{{/if}}" type="${msgItem.content_type}">\
-                                                {{if msgItem.image_url}}<img src="${msgItem.image_url}">{{/if}} <span class="quickreplyText {{if msgItem.image_url}}with-img{{/if}}">${msgItem.title}</span></span>\
-                                            </div> \
-                                        {{/each}} \
-                                    </div>\
+                                {{if msgData.message[0].cInfo && msgData.message[0].cInfo.emoji}} \
+                                    <span class="emojione emojione-${msgData.message[0].cInfo.emoji[0].code}">${msgData.message[0].cInfo.emoji[0].title}</span> \
+                                {{/if}} \
+                            </div>\
+                        {{/if}} \
+                        {{if msgData.message[0].component.payload.quick_replies && msgData.message[0].component.payload.quick_replies.length}} \
+                            <div class="quick_replies_btn_parent">\
+                                <div class="gridContainer">\
+                                    {{each(key, msgItem) msgData.message[0].component.payload.quick_replies}} \
+                                        <div class="buttonTmplContentChild quickReplyDiv">\
+                                            <span {{if msgItem.payload}}value="${msgItem.payload}"{{/if}} class="quickReply {{if msgItem.image_url}}with-img{{/if}}" type="${msgItem.content_type}">\
+                                                {{if msgItem.image_url}}<img src="${msgItem.image_url}">{{/if}}\
+                                                <span class="quickreplyText {{if msgItem.image_url}}with-img{{/if}}">${msgItem.title}</span>\
+                                            </span>\
+                                        </div> \
+                                    {{/each}} \
                                 </div>\
-                            {{/if}} \
-                        </div>\
-                    </li> \
-                {{/if}} \
-	    {{if msgData.createdOn}}\
-		    <div aria-live="off" class="extra-info" style="margin-right: 15px; margin-top: -10px; margin-bottom: 3px; margin-left: 48px; font-size: 12px; color: #8a959f;">\
-		    ${helpers.formatDate(msgData.createdOn)}</div>\
-		{{/if}} \
-            </scipt>';
+                            </div>\
+                        {{/if}} \
+                    </div> \
+                </li> \
+            {{/if}} \
+            {{if msgData.createdOn}}\
+                <div aria-live="off" class="extra-info" style="margin-right: 15px; margin-top: -10px; margin-bottom: 3px; margin-left: 48px; font-size: 12px; color: #8a959f;">\
+                ${helpers.formatDate(msgData.createdOn)}</div>\
+            {{/if}} \
+        </script>';
+  //           var quickReplyTemplate = '<script id="chat_message_tmpl" type="text/x-jqury-tmpl"> \
+  //               {{if msgData.message}} \
+  //                   <li data-time="${msgData.createdOnTimemillis}" id="${msgData.messageId || msgItem.clientMessageId}"\
+  //                       class="{{if msgData.type === "bot_response"}}fromOtherUsers{{else}}fromCurrentUser{{/if}} with-icon quickReplies"> \
+  //                       <div class="buttonTmplContent"> \
+  //                           {{if msgData.icon}}<div aria-live="off" class="profile-photo"> <div class="user-account avtar marginT50" style="background-image:url(${msgData.icon})"></div> </div> {{/if}} \
+  //                           {{if msgData.message[0].component.payload.text}} \
+  //                               <div class="buttonTmplContentHeading quickReply"> \
+  //                                   {{if msgData.type === "bot_response"}} {{html helpers.convertMDtoHTML(msgData.message[0].component.payload.text, "bot")}} {{else}} {{html helpers.convertMDtoHTML(msgData.message[0].component.payload.text, "user")}} {{/if}} \
+  //                                   {{if msgData.message[0].cInfo && msgData.message[0].cInfo.emoji}} \
+  //                                       <span class="emojione emojione-${msgData.message[0].cInfo.emoji[0].code}">${msgData.message[0].cInfo.emoji[0].title}</span> \
+  //                                   {{/if}} \
+  //                               </div>\
+  //                               {{/if}} \
+  //                               {{if msgData.message[0].component.payload.quick_replies && msgData.message[0].component.payload.quick_replies.length}} \
+  //                               <div class="fa fa-chevron-left quickreplyLeftIcon hide"></div><div class="fa fa-chevron-right quickreplyRightIcon"></div>\
+  //                                   <div class="quick_replies_btn_parent"><div class="autoWidth">\
+  //                                       {{each(key, msgItem) msgData.message[0].component.payload.quick_replies}} \
+  //                                           <div class="buttonTmplContentChild quickReplyDiv"> <span {{if msgItem.payload}}value="${msgItem.payload}"{{/if}} class="quickReply {{if msgItem.image_url}}with-img{{/if}}" type="${msgItem.content_type}">\
+  //                                               {{if msgItem.image_url}}<img src="${msgItem.image_url}">{{/if}} <span class="quickreplyText {{if msgItem.image_url}}with-img{{/if}}">${msgItem.title}</span></span>\
+  //                                           </div> \
+  //                                       {{/each}} \
+  //                                   </div>\
+  //                               </div>\
+  //                           {{/if}} \
+  //                       </div>\
+  //                   </li> \
+  //               {{/if}} \
+	 //    {{if msgData.createdOn}}\
+		//     <div aria-live="off" class="extra-info" style="margin-right: 15px; margin-top: -10px; margin-bottom: 3px; margin-left: 48px; font-size: 12px; color: #8a959f;">\
+		//     ${helpers.formatDate(msgData.createdOn)}</div>\
+		// {{/if}} \
+  //           </scipt>';
             var listTemplate = '<script id="chat_message_tmpl" type="text/x-jqury-tmpl"> \
                 {{if msgData.message}} \
                     <li data-time="${msgData.createdOnTimemillis}" id="${msgData.messageId || msgItem.clientMessageId}"\
