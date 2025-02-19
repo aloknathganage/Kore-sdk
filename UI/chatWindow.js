4baceb7e73d2a9650590ef386413b7f0b064d90a
AnuragKule <89693418+AnuragKule@users.noreply.github.com>
2025-02-19 12:21:02 +0530

    Update chatWindow.js

--- UI/chatWindow.js
+++ UI/chatWindow.js
@@ -3255,11 +3255,11 @@
                     if (msgData.message[0]?.component?.payload?.template_type == 'quickReplyTemplate') {
                         $(".buttonTmplContentChild.quickReplyDiv")
                             .css("pointer-events", "auto")
-                            .css("opacity", "1.1");
+                            .css("opacity", "0.8");
                     } else {
                         $(".buttonTmplContentChild.quickReplyDiv")
                             .css("pointer-events", "none")
-                            .css("opacity", "1.1");
+                            .css("opacity", "0.8");
                     }
                     //pallavi disable quickreply 13/02
                     if(msgData.message[0]?.component?.payload?.template_type == 'table'){
